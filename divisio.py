7c5d1b3126975d36bdd47600d8a85ebbc179e0e9
OriolMangues <omangues@instituticaria.cat>
2024-10-16 13:49:16 +0200

    divisio

--- divisio.py
+++ divisio.py
@@ -1,11 +1,7 @@
-print('aquest programa realitza una divisio')
-a=float(input('divisor'))
-b=float(input('dividend'))
-D=b//a
-r=b%a
-if a==0:
-    print('no es por realitzar una divisió entre 0')
-else: 
-    print('divisio:',b,'/',a)
-    print('quocient:',D)
-    print('residu:',r)
+Dividend = int(input("Divident:"))
+Divisor = int(input("Divisor:"))
+Quocient = Divident // Divisor
+Residu = Divident % Divisor
+print(f"Divisió: {Divident}/{Divisor}")
+print(f"Quocient: {Quocient}")
+print(f"Residu: {Residu}")
